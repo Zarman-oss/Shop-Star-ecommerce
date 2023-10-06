ad53ca3db82f7a76816d0c6be13977365c8bcc6c
zarman imran <zarmanimran@gmail.com>
2023-10-07 01:29:40 +0500

    first commit of the day 10.7.2023

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -1,9 +1,26 @@
+import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 
-
-const CheckoutSteps = ({step1,}) => {
+const CheckoutSteps = ({ step1, step2, step3 }) => {
   return (
-    <div>CheckoutSteps</div>
-  )
-}
+    <nav className="flex justify-center mb-4">
+      <div className="nav-item">
+        {step1 ? (
+          <Link to="/login" className="nav-link">
+            Sign In
+          </Link>
+        ) : (
+          <span className="nav-link-disabled">Sign In</span>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default CheckoutSteps
+// CheckoutSteps.propTypes = {
+//   step1: PropTypes.bool.isRequired,
+//   step2: PropTypes.bool.isRequired,
+//   step3: PropTypes.bool.isRequired,
+// };
+
+export default CheckoutSteps;
