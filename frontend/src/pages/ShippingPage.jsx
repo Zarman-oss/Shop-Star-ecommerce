b291b99f842b2bce62da35ea6153b6ef24588d32
zarman imran <zarmanimran@gmail.com>
2023-10-06 07:41:17 +0500

    private routes 10 6 2023

--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 import FormContainer from '../components/FormContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { saveShippingAddress } from '../slices/cartSlice';
 
 const ShippingPage = () => {
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [postalCode, setCode] = useState('');
-  const [country, setCountry] = useState('');
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress?.address || '');
+
+  const [city, setCity] = useState(shippingAddress?.city || '');
+
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress?.postalCode || ''
+  );
+  const [country, setCountry] = useState(shippingAddress?.country || '');
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('submit');
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate('/payment');
   };
 
   return (
@@ -50,6 +64,47 @@ const ShippingPage = () => {
             className="border rounded-md py-2 px-3 mt-1 w-full placeholder-gray-400 focus:outline-none focus:ring focus:border-blue-300 text-sm"
           />
         </div>
+        {/* Postal Code */}
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            <h2 className="text-2xl">Postal Code</h2>
+          </label>
+          <input
+            id="postal code"
+            type="text"
+            placeholder="Enter city"
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+            className="border rounded-md py-2 px-3 mt-1 w-full placeholder-gray-400 focus:outline-none focus:ring focus:border-blue-300 text-sm"
+          />
+        </div>
+        {/* Country  */}
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            <h2 className="text-2xl">Country</h2>
+          </label>
+          <input
+            id="country"
+            type="text"
+            placeholder="Enter country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            className="border rounded-md py-2 px-3 mt-1 w-full placeholder-gray-400 focus:outline-none focus:ring focus:border-blue-300 text-sm"
+          />
+        </div>
+        {/* Continue Button */}
+        <button
+          type="submit"
+          className="bg-gray-700 hover:bg-gray-500 text-white font-semibold py-3 px-4 rounded transition duration-300 ease-in-out text-xs"
+        >
+          Continue
+        </button>
       </form>
     </FormContainer>
   );
