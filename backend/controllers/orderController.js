089225616c95ca097286710f12e35a7599d179f6
zarman imran <zarmanimran@gmail.com>
2023-10-11 11:28:47 +0500

    .

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,8 +8,39 @@ import Order from '../models/orderModel.js';
 
 const addOrderItems = asyncHandler(async (req, res) => {
 
-    res.send('add order items for now')
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
 
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items');
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined
+            })),
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        })
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
 });
 
 
@@ -18,9 +49,8 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-
-    res.send('get my orders')
-
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
 });
 
 
@@ -29,7 +59,14 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @access Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-    res.json('add order by Id');
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (order) {
+        res.status(200).json(order)
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 
 });
 
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -10,11 +10,10 @@ import {
 } from '../controllers/OrderController.js';
 import { protect, admin } from '../middleware/authMiddle.js'
 
-
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/mine').get(protect, getMyOrders);
 router.route('/:id').get(protect, admin, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
-export default router;
+export default router;  
