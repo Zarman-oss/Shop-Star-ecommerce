01b2a613724e64560af087c5ae423b4c93ed40d2
zarman imran <zarmanimran@gmail.com>
2023-10-12 02:57:50 +0500

    .

--- frontend/src/pages/PlaceOrderPage.jsx
+++ frontend/src/pages/PlaceOrderPage.jsx
@@ -1,12 +1,29 @@
 import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const PlaceOrderPage = () => {
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate('/shipping');
+    } else if (!cart.paymentMethod) {
+      navigate('/payment');
+    }
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
+
   return (
-    <div>
-      <h1 className="text-4xl font-semibold mb-6 text-center">
-        PlaceOrderPage
-      </h1>
-    </div>
+    <>
+      <div>
+        <CheckoutSteps step1 step2 step3 step4 />
+        <h1 className="text-4xl font-semibold mb-6 text-center">
+          PlaceOrderPage
+        </h1>
+      </div>
+    </>
   );
 };
 
