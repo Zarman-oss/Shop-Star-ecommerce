ea70c8863c6a28650f4849509cb3c0e669dd32db
zarman imran <zarmanimran@gmail.com>
2023-10-11 08:25:39 +0500

    10.11.2023

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-import userRoutes from './routes/orderRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
