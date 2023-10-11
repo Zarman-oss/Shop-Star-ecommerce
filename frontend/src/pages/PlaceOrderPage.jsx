d53d742ed7a5b75d6f0f6331ca6f5b60983d3416
zarman imran <zarmanimran@gmail.com>
2023-10-12 03:29:13 +0500

    .

--- frontend/src/pages/PlaceOrderPage.jsx
+++ frontend/src/pages/PlaceOrderPage.jsx
@@ -2,6 +2,11 @@ import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { toast } from 'react-toastify';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { useCreateOrderMutation } from '../slices/ordersApiSlice';
+import { clearCartItems } from '../slices/cartSlice';
 
 const PlaceOrderPage = () => {
   const navigate = useNavigate();
@@ -22,6 +27,20 @@ const PlaceOrderPage = () => {
         <h1 className="text-4xl font-semibold mb-6 text-center">
           PlaceOrderPage
         </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="md:col-span-1">
+            <h2 className="text-3xl font-bold">Shipping</h2>
+            <p>
+              <h1 className="text-3xl font-bold">Address:</h1>
+              {cart.shippingAddress.address},{cart.shippingAddress.city}, {''}
+              {cart.shippingAddress.postalCode},{''}
+              {cart.shippingAddress.country}
+            </p>
+          </div>
+          <div className="md:col-span-1">
+            <div className="text-lg">Column</div>
+          </div>
+        </div>
       </div>
     </>
   );
