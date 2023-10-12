c1cfa32c4a533d915cda61eff46d9c23a097e8e9
zarman imran <zarmanimran@gmail.com>
2023-10-12 08:21:43 +0500

    .

--- frontend/src/pages/PlaceOrderPage.jsx
+++ frontend/src/pages/PlaceOrderPage.jsx
@@ -7,6 +7,8 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { useCreateOrderMutation } from '../slices/ordersApiSlice';
 import { clearCartItems } from '../slices/cartSlice';
+import { FaPaypal } from 'react-icons/fa';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const PlaceOrderPage = () => {
   const navigate = useNavigate();
@@ -34,10 +36,12 @@ const PlaceOrderPage = () => {
             <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">
               Shipping
             </h2>
-            <p>
-              <h1 className="text-lg md:text-lg lg:text-xl font-semibold">
+            <p className="flex items-center">
+              <h1 className="text-lg md:text-lg lg:text-xl font-semibold mr-1">
                 Address:
               </h1>
+              <br />
+              <FaLocationArrow className="text-xl md:text-2xl lg:text-3xl mr-1" />
               <span className="text-sm md:text-base lg:text-lg">
                 {cart.shippingAddress.address}, {cart.shippingAddress.city},{' '}
                 {cart.shippingAddress.postalCode},{' '}
@@ -46,23 +50,30 @@ const PlaceOrderPage = () => {
             </p>
           </div>
         </div>
-        {/* second */}
+        {/* Payment Method */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="md:col-span-1">
-            <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-bold ">
               Payment Method
             </h2>
-            <p>
-              <h1 className="text-lg md:text-lg lg:text-xl font-semibold">
+            <p className="flex items-center">
+              <h1 className="text-lg md:text-lg lg:text-xl font-semibold mr-1">
                 Method:
               </h1>
+              <FaPaypal className="text-xl md:text-2xl lg:text-3xl mr-2" />
               <span className="text-sm md:text-base lg:text-lg">
                 {cart.paymentMethod}
               </span>
             </p>
           </div>
         </div>
-        {/*  */}
+        {/* Order Items */}
+        <h2>Order Items</h2>
+        {cart.cartItems.length === 0 ? ( 
+       <Message>Your Cart is empty</Message>
+        ) : (
+         
+        )}
       </div>
     </>
   );
