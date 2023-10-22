f6858579c22698ef179d0d0ceff9d8b73f9e2b16
zarman imran <zarmanimran@gmail.com>
2023-10-22 09:06:13 +0500

    .

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -71,18 +71,34 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc Update order to paid
-// @route POST /api/orders/:id/pay
+// @route PUT /api/orders/:id/pay
 // @access Private
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
 
-    res.send('update order to paid')
+    const order = await Order.findById(req.params.id)
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        };
+
+        const updatedOrder = await order.save();
+        res.status(200).json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found ')
+    }
 
 });
 
 
 // @desc Update order to delivered
-// @route POST /api/orders/:id/deliver
+// @route PUT /api/orders/:id/deliver
 // @access Private/Admin
 
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
