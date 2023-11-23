f4bb65316676b6394b4f5f9eefc988ea56b3f3ae
Zarman-oss <zarmanimran@gmail.com>
2023-11-23 22:17:30 +0500

    ,

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,12 @@ const __dirname = path.resolve();
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+// Build Config
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
 
 } else {
     app.get('/', (req, res) => {
