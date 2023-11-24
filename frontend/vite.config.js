86d4eabf842bf18cf09cd6573d6347628e052360
Zarman-oss <zarmanimran@gmail.com>
2023-11-24 09:09:34 +0500

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/src/main.jsx'
-    },
-  },
+
   plugins: [react()],
   server: {
     proxy: {
