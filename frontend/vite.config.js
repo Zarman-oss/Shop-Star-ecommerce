aaf15db786939a4ded42c110736d69b0a4dcd78d
Zarman-oss <zarmanimran@gmail.com>
2023-11-25 04:05:49 +0500

    .,

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig=paths'
 // https://vitejs.dev/config/
 
 export default defineConfig({
 
 
-  build: {
-    rollupOptions: {
-      // https://rollupjs.org/configuration-options/
-    },
-  },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     proxy: {
       '/api': {
