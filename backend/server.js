750337791996708c7a59695d7d0e6e3f3a4ec5fd
Zarman-oss <zarmanimran@gmail.com>
2023-12-24 03:42:46 +0500

    static folder for images

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 
   const __dirname = path.resolve();
 
-  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 
 
