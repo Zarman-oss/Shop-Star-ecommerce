c18018e3d89d38befb71d0a47706ebb466afa49a
Zarman-oss <zarmanimran@gmail.com>
2024-01-18 03:05:39 +0500

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; 
+import  color  from "colors";
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected : ${conn.connection.host}`)
+        console.log(`MongoDB Connected : ${conn.connection.host}`.yellow.bold);
     } catch (error) {
         console.log(`Error: ${error.message}`)
         process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; 
+import colors from 'colors'
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -11,9 +12,7 @@ import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
-
 connectDB();
-
 const app = express();
 
 app.use(express.json());
@@ -58,7 +57,7 @@ app.use(errorHandler);
 
 
 app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.magenta.bold)
 );
   
 
